3743ce05d42baaae4c9ab5305bc72c95471a6c57
Dave Sherlock <dave.sherlock@gmail.com>
2016-05-11 10:20:40 +0100

    Update backupIIS.bat

--- backupIIS.bat
+++ backupIIS.bat
@@ -1,4 +1,11 @@
+rem set the current date
 set currdate=%date:/=%
+
+rem set the path name
 set Pathname="C:\Windows\System32\inetsrv"
+
+rem direcroty to the pathname
 cd /d %Pathname%
+
+rem run the command
 appcmd add backup backup_%currdate%
